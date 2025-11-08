ba888369a1b770f870623d102eeecba210f1815d
Denny Christochowitz <17357410+dchowitz@users.noreply.github.com>
2025-11-08 17:35:43 +0100

    adjust exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export * from './repo';
+export type { Repo } from './repo';
+export { createMongoRepo } from './mongo-repo';
+export { createFirestoreRepo } from './firestore-repo';
 export { QueryStream } from './query-stream';
